db30638294ff1b9ad273308eaeae211a964d12ca
ArtIKim <null_number_13@hotmail.com>
2021-04-09 13:20:04 +0300

    09042021

--- src/components/Add.js
+++ src/components/Add.js
@@ -1,25 +1,39 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 export const Add = () => {
-    const [query, setQuery] = useState("");
+	const [query, setQuery] = useState("");
 
-    const onChange = (e) => {
-        e.preventDefault();
+	const getMovies = async (query) => {
+		try {
+			const response = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1&include_adult=false&query=${query}`);
+			// const response = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1&include_adult=false&query=${query}`);
+			const data = await response.json();
 
-        setQuery(e.target.value);
+			return data;
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
-        try {
-            const response = fetch(`https//:api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1&include_adult=false$query=${e.target.value}`);
-            const data = response.json();
-            console.log(data);
-        } catch (error) {
-            console.log(error);
-        }
-    };
+	const onChange = async (e) => {
+		e.preventDefault();
 
-    return (
-        <div className="max-w-600 mx-auto px-20 py-80">
-            <input className="w-full h-44 p-10" type="text" placeholder="Search for a movie..." value={query} onChange={onChange} />
-        </div>
-    );
+		setQuery(e.target.value);
+
+		const results = await getMovies(e.target.value);
+		console.log(results);
+
+		// fetch(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1&include_adult=false&query=${e.target.value}`)
+		// 	.then((res) => res.json())
+		// 	.then((data) => {
+		// 		console.log(data);
+		// 	});
+	};
+
+	return (
+		<div className="max-w-600 mx-auto px-20 py-80">
+			<input className="w-full h-44 p-10" type="text" placeholder="Search for a movie..." value={query} onChange={onChange} />
+		</div>
+	);
 };
